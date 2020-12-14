b481fdb36a03fb0813f6b3afc0e8f384611de7da
Lizaveta-PAS <elizaveta.tsvirko@icloud.com>
2020-12-14 13:11:31 +0300

    Grant update (remote access)

--- sql/2_create_database.sql
+++ sql/2_create_database.sql
@@ -1,7 +1,11 @@
 CREATE DATABASE musicShop_db DEFAULT CHARACTER SET utf8;
 
-CREATE USER shop_user@localhost IDENTIFIED BY 'shop_password';
-
+CREATE USER 'shop_user'@'localhost' IDENTIFIED BY 'shop_password';
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER
     ON musicShop_db.*
     TO shop_user@localhost;
+
+CREATE USER 'shop_user'@'%' IDENTIFIED BY 'shop_password';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER
+    ON musicShop_db.*
+    TO shop_user@'%';
