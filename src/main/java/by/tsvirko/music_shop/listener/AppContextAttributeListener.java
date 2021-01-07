342a6bff5b0099380531d034b568fccd1254a83a
Lizaveta-PAS <elizaveta.tsvirko@icloud.com>
2021-01-07 22:54:24 +0300

    simple listeners implemented

---
+++ src/main/java/by/tsvirko/music_shop/listener/AppContextAttributeListener.java
@@ -0,0 +1,26 @@
+package by.tsvirko.music_shop.listener;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletContextAttributeEvent;
+import javax.servlet.ServletContextAttributeListener;
+
+/**
+ * Logs the event when attribute is added, removed or replaced in servlet context
+ */
+public class AppContextAttributeListener implements ServletContextAttributeListener {
+    private static final Logger logger = LogManager.getLogger(AppContextAttributeListener.class);
+
+    public void attributeAdded(ServletContextAttributeEvent servletContextAttributeEvent) {
+        logger.info("ServletContext attribute added::{" + servletContextAttributeEvent.getName() + "," + servletContextAttributeEvent.getValue() + "}");
+    }
+
+    public void attributeReplaced(ServletContextAttributeEvent servletContextAttributeEvent) {
+        logger.info("ServletContext attribute replaced::{" + servletContextAttributeEvent.getName() + "," + servletContextAttributeEvent.getValue() + "}");
+    }
+
+    public void attributeRemoved(ServletContextAttributeEvent servletContextAttributeEvent) {
+        logger.info("ServletContext attribute removed::{" + servletContextAttributeEvent.getName() + "," + servletContextAttributeEvent.getValue() + "}");
+    }
+}
--- src/main/java/by/tsvirko/music_shop/listener/RequestListener.java
+++ src/main/java/by/tsvirko/music_shop/listener/RequestListener.java
@@ -1,4 +1,26 @@
 package by.tsvirko.music_shop.listener;
 
-public class RequestListener {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Track the events of creating a request when accessing the servlet and its destruction
+ */
+public class RequestListener implements ServletRequestListener {
+    private static final Logger logger = LogManager.getLogger(RequestListener.class);
+
+    public void requestInitialized(ServletRequestEvent ev) {
+        HttpServletRequest req = (HttpServletRequest) ev.getServletRequest();
+        String uri = req.getRequestURI();
+        String id = req.getRequestedSessionId();
+        logger.info("Request Initialized for " + uri + "\n" + "Request Initialized with ID= " + id);
+    }
+
+    public void requestDestroyed(ServletRequestEvent ev) {
+        logger.info("Request Destroyed: " + ev.getServletRequest().getAttribute("lifecycle"));
+    }
 }
