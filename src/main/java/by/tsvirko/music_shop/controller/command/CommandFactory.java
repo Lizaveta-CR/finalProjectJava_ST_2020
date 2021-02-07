1f14b71b7f94d8b8213039ff4c6c0c8adf7f2d1f
Lizaveta-PAS <elizaveta.tsvirko@icloud.com>
2021-02-07 11:40:52 +0300

    Command name factory

---
+++ src/main/java/by/tsvirko/music_shop/controller/command/CommandFactory.java
@@ -0,0 +1,141 @@
+package by.tsvirko.music_shop.controller.command;
+
+import by.tsvirko.music_shop.controller.command.exception.CommandException;
+import by.tsvirko.music_shop.controller.command.impl.admin.*;
+import by.tsvirko.music_shop.controller.command.impl.buyer.*;
+import by.tsvirko.music_shop.controller.command.impl.common.*;
+import by.tsvirko.music_shop.controller.command.impl.manager.*;
+
+/**
+ * Command factory class.
+ */
+public class CommandFactory {
+    private static class Holder {
+        private static final CommandFactory INSTANCE = new CommandFactory();
+    }
+
+    public static CommandFactory getInstance() {
+        return CommandFactory.Holder.INSTANCE;
+    }
+
+
+    /**
+     * Gets commands by name.
+     *
+     * @param name - command name
+     * @return Command corresponding to name
+     */
+    private static Command getCommandByName(CommandName name) {
+        switch (name) {
+            //common commands
+            case ABOUT_VIEW_COMMAND:
+                return new AboutViewCommand();
+            case CATEGORIES_COMMAND:
+                return new CategoriesCommand();
+            case CHANGE_LANGUAGE_COMMAND:
+                return new ChangeLanguageCommand();
+            case LOGIN_COMMAND:
+                return new LoginCommand();
+            case LOGOUT_COMMAND:
+                return new LogoutCommand();
+            case MAIN_COMMAND:
+                return new MainCommand();
+            case REGISTER_COMMAND:
+                return new RegisterCommand();
+            case SHOW_PRODUCER_PRODUCTS_COMMAND:
+                return new ShowProducerProductsCommand();
+            case VIEW_REGISTER_COMMAND:
+                return new ViewRegisterCommand();
+            case VIEW_LOGIN_COMMAND:
+                return new ViewLoginCommand();
+            //buyer commands
+            case BUYER_ADD_PRODUCT_COMMAND:
+                return new BuyerAddProductCommand();
+            case BUYER_EDIT_COMMAND:
+                return new BuyerEditCommand();
+            case BUYER_EDIT_PASS_COMMAND:
+                return new BuyerEditPassCommand();
+            case BUYER_FORM_COMMAND:
+                return new BuyerFormCommand();
+            case BUYER_PRODUCTS_BY_RATE_COMMAND:
+                return new BuyerProductsByRateCommand();
+            case BUYER_REMOVE_PRODUCT_COMMAND:
+                return new BuyerRemoveProductCommand();
+            case BUYER_VIEW_EDIT_FORM_COMMAND:
+                return new BuyerViewEditFormCommand();
+            case BUYER_VIEW_EDIT_PASS_FORM_COMMAND:
+                return new BuyerViewEditPassFormCommand();
+            case BUYER_VIEW_SUBMIT_ORDER_COMMAND:
+                return new BuyerViewSubmitOrderCommand();
+            case EDIT_ADDRESS_COMMAND:
+                return new EditAddressCommand();
+            case SUBMIT_FEEDBACK_COMMAND:
+                return new SubmitFeedBackCommand();
+            case SUBMIT_ORDER_COMMAND:
+                return new SubmitOrderCommand();
+            case VIEW_ADDRESS_COMMAND:
+                return new ViewAddressCommand();
+            case VIEW_FEEDBACK_PAGE:
+                return new ViewFeedBackPage();
+            case VIEW_ORDER_COMMAND:
+                return new ViewOrderCommand();
+            //admin commands
+            case ADMIN_BUYERS_COMMAND:
+                return new AdminBuyersCommand();
+            case ADMIN_EDIT_PRODUCTS_COMMAND:
+                return new AdminEditProductsCommand();
+            case ADMIN_EDIT_PRODUCTS_VIEW_COMMAND:
+                return new AdminEditProductsViewCommand();
+            case ADMIN_SEND_MAIL_COMMAND:
+                return new AdminSendMailCommand();
+            case DISABLE_ACCESS_COMMAND:
+                return new DisableAccessCommand();
+            case ENABLE_ACCESS_COMMAND:
+                return new EnableAccessCommand();
+            case ENABLE_PRODUCT_COMMAND:
+                return new EnableProductCommand();
+            case GENERATE_BUYER_COMMAND:
+                return new GenerateBuyerCommand();
+            case SHOW_UNAVAILABLE_PRODUCTS_COMMAND:
+                return new ShowUnavailableProductsCommand();
+            case SHOW_WINNER_COMMAND:
+                return new ShowWinnerCommand();
+            //manager commands
+            case ADD_EMPLOYEE_COMMAND:
+                return new AddEmployeeCommand();
+            case ADD_PRODUCT_COMMAND:
+                return new AddProductCommand();
+            case DELETE_EMPLOYEE_COMMAND:
+                return new DeleteEmployeeCommand();
+            case VIEW_ADD_PERSONAL_COMMAND:
+                return new ViewAddPersonalCommand();
+            case VIEW_ADD_PRODUCT_COMMAND:
+                return new ViewAddProductCommand();
+            case VIEW_EARNINGS_COMMAND:
+                return new ViewEarningsCommand();
+            case VIEW_PERSONAL_COMMAND:
+                return new ViewPersonalCommand();
+            case DELETE_PRODUCT_COMMAND:
+                return new DeleteProductCommand();
+            default:
+                throw new IllegalArgumentException("No such command");
+        }
+
+    }
+
+    /**
+     * Returns object of command class which corresponds to command name.
+     *
+     * @return command corresponding to command name
+     * @throws CommandException if command wasn't found
+     */
+    public static Command getCommand(CommandName commandName) throws CommandException {
+        Command command = null;
+        try {
+            command = getCommandByName(commandName);
+            return command;
+        } catch (IllegalArgumentException e) {
+            throw new CommandException(e);
+        }
+    }
+}
--- src/main/java/by/tsvirko/music_shop/controller/filter/CommandFilter.java
+++ src/main/java/by/tsvirko/music_shop/controller/filter/CommandFilter.java
@@ -1,13 +1,11 @@
 package by.tsvirko.music_shop.controller.filter;
 
+import by.tsvirko.music_shop.controller.command.CommandFactory;
+import by.tsvirko.music_shop.controller.command.CommandName;
 import by.tsvirko.music_shop.controller.command.constant.AttributeConstant;
 import by.tsvirko.music_shop.controller.command.constant.PathConstant;
 import by.tsvirko.music_shop.controller.command.Command;
-//import by.tsvirko.music_shop.controller.command.impl.buyer.BuyerEditCommand;
-import by.tsvirko.music_shop.controller.command.impl.admin.*;
-import by.tsvirko.music_shop.controller.command.impl.buyer.*;
-import by.tsvirko.music_shop.controller.command.impl.common.*;
-import by.tsvirko.music_shop.controller.command.impl.manager.*;
+import by.tsvirko.music_shop.controller.command.exception.CommandException;
 import by.tsvirko.music_shop.service.util.ResourceBundleUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -25,70 +23,72 @@ import java.util.concurrent.ConcurrentHashMap;
 public class CommandFilter implements Filter {
     private static final Logger logger = LogManager.getLogger(CommandFilter.class);
 
-    private static Map<String, Command> getCommands = new ConcurrentHashMap<>();
-    private static Map<String, Command> postCommands = new ConcurrentHashMap<>();
-
-    static {
-        getCommands.put("/", new MainCommand());
-        getCommands.put("/index", new MainCommand());
-        getCommands.put("/welcome", new MainCommand());
-
-        getCommands.put("/changeLocale", new ChangeLanguageCommand());
-        getCommands.put("/login", new ViewLoginCommand());
-        getCommands.put("/logout", new LogoutCommand());
-        getCommands.put("/registration", new ViewRegisterCommand());
-        getCommands.put("/about", new AboutViewCommand());
-
-        getCommands.put("/products/list", new CategoriesCommand());
-        getCommands.put("/products/edit", new AdminEditProductsViewCommand());
-        getCommands.put("/products/producer", new ShowProducerProductsCommand());
-
-        getCommands.put("/admin/buyers", new AdminBuyersCommand());
-        getCommands.put("/admin/unavailableProducts", new ShowUnavailableProductsCommand());
-        getCommands.put("/admin/winner", new ShowWinnerCommand());
-
-        getCommands.put("/buyer/buyerForm", new BuyerFormCommand());
-        getCommands.put("/buyer/feedBack", new ViewFeedBackPage());
-        getCommands.put("/buyer/edit", new BuyerViewEditFormCommand());
-        getCommands.put("/buyer/editPass", new BuyerViewEditPassFormCommand());
-        getCommands.put("/buyer/address", new ViewAddressCommand());
-        getCommands.put("/buyer/order", new ViewOrderCommand());
-        getCommands.put("/buyer/order/submit", new BuyerViewSubmitOrderCommand());
-
-        getCommands.put("/manag/earnings", new ViewEarningsCommand());
-        getCommands.put("/manag/personal", new ViewPersonalCommand());
-        getCommands.put("/manag/add-personal", new ViewAddPersonalCommand());
-        getCommands.put("/products/create", new ViewAddProductCommand());
-
-        postCommands.put("/login", new LoginCommand());
-        postCommands.put("/registration", new RegisterCommand());
-
-        postCommands.put("/buyer/edit", new BuyerEditCommand());
-        postCommands.put("/buyer/editPass", new BuyerEditPassCommand());
-        postCommands.put("/buyer/address", new EditAddressCommand());
-        postCommands.put("/buyer/order/remove", new BuyerRemoveProductCommand());
-        postCommands.put("/buyer/order/submit", new SubmitOrderCommand());
-        postCommands.put("/buyer/buyerForm", new BuyerFormCommand());
-        postCommands.put("/buyer/feedBack/submit", new SubmitFeedBackCommand());
-        postCommands.put("/products/buy", new BuyerAddProductCommand());
-        postCommands.put("/products/create", new AddProductCommand());
-        postCommands.put("/products/delete", new DeleteProductCommand());
-        postCommands.put("/products/byRate", new BuyerProductsByRateCommand());
-
-        postCommands.put("/admin/buyers", new AdminBuyersCommand());
-        postCommands.put("/admin/buyers/disable", new DisableAccessCommand());
-        postCommands.put("/admin/buyers/enable", new EnableAccessCommand());
-        postCommands.put("/admin/unavailableProducts", new EnableProductCommand());
-        postCommands.put("/admin/generate", new GenerateBuyerCommand());
-        postCommands.put("/admin/mail", new AdminSendMailCommand());
-        postCommands.put("/products/edit", new AdminEditProductsCommand());
-
-        postCommands.put("/manag/add-personal", new AddEmployeeCommand());
-        postCommands.put("/manag/delete", new DeleteEmployeeCommand());
-    }
+    private static Map<String, CommandName> getCommands = new ConcurrentHashMap<>();
+    private static Map<String, CommandName> postCommands = new ConcurrentHashMap<>();
+    private CommandFactory factory;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
+        factory = CommandFactory.getInstance();
+    }
+
+    static {
+        getCommands.put("/", CommandName.MAIN_COMMAND);
+        getCommands.put("/index", CommandName.MAIN_COMMAND);
+        getCommands.put("/welcome", CommandName.MAIN_COMMAND);
+
+        getCommands.put("/changeLocale", CommandName.CHANGE_LANGUAGE_COMMAND);
+        getCommands.put("/login", CommandName.VIEW_LOGIN_COMMAND);
+        getCommands.put("/logout", CommandName.LOGOUT_COMMAND);
+        getCommands.put("/registration", CommandName.VIEW_REGISTER_COMMAND);
+        getCommands.put("/about", CommandName.ABOUT_VIEW_COMMAND);
+
+        getCommands.put("/products/list", CommandName.CATEGORIES_COMMAND);
+        getCommands.put("/products/edit", CommandName.ADMIN_EDIT_PRODUCTS_VIEW_COMMAND);
+        getCommands.put("/products/producer", CommandName.SHOW_PRODUCER_PRODUCTS_COMMAND);
+
+        getCommands.put("/admin/buyers", CommandName.ADMIN_BUYERS_COMMAND);
+        getCommands.put("/admin/unavailableProducts", CommandName.SHOW_UNAVAILABLE_PRODUCTS_COMMAND);
+        getCommands.put("/admin/winner", CommandName.SHOW_WINNER_COMMAND);
+
+        getCommands.put("/buyer/buyerForm", CommandName.BUYER_FORM_COMMAND);
+        getCommands.put("/buyer/feedBack", CommandName.VIEW_FEEDBACK_PAGE);
+        getCommands.put("/buyer/edit", CommandName.BUYER_VIEW_EDIT_FORM_COMMAND);
+        getCommands.put("/buyer/editPass", CommandName.BUYER_VIEW_EDIT_PASS_FORM_COMMAND);
+        getCommands.put("/buyer/address", CommandName.VIEW_ADDRESS_COMMAND);
+        getCommands.put("/buyer/order", CommandName.VIEW_ORDER_COMMAND);
+        getCommands.put("/buyer/order/submit", CommandName.BUYER_VIEW_SUBMIT_ORDER_COMMAND);
+
+        getCommands.put("/manag/earnings", CommandName.VIEW_EARNINGS_COMMAND);
+        getCommands.put("/manag/personal", CommandName.VIEW_PERSONAL_COMMAND);
+        getCommands.put("/manag/add-personal", CommandName.VIEW_ADD_PERSONAL_COMMAND);
+        getCommands.put("/products/create", CommandName.VIEW_ADD_PRODUCT_COMMAND);
+
+        postCommands.put("/login", CommandName.LOGIN_COMMAND);
+        postCommands.put("/registration", CommandName.REGISTER_COMMAND);
+
+        postCommands.put("/buyer/edit", CommandName.BUYER_EDIT_COMMAND);
+        postCommands.put("/buyer/editPass", CommandName.BUYER_EDIT_PASS_COMMAND);
+        postCommands.put("/buyer/address", CommandName.EDIT_ADDRESS_COMMAND);
+        postCommands.put("/buyer/order/remove", CommandName.BUYER_REMOVE_PRODUCT_COMMAND);
+        postCommands.put("/buyer/order/submit", CommandName.SUBMIT_ORDER_COMMAND);
+        postCommands.put("/buyer/buyerForm", CommandName.BUYER_FORM_COMMAND);
+        postCommands.put("/buyer/feedBack/submit", CommandName.SUBMIT_FEEDBACK_COMMAND);
+        postCommands.put("/products/buy", CommandName.BUYER_ADD_PRODUCT_COMMAND);
+        postCommands.put("/products/create", CommandName.ADD_PRODUCT_COMMAND);
+        postCommands.put("/products/delete", CommandName.DELETE_PRODUCT_COMMAND);
+        postCommands.put("/products/byRate", CommandName.BUYER_PRODUCTS_BY_RATE_COMMAND);
+
+        postCommands.put("/admin/buyers", CommandName.ADMIN_BUYERS_COMMAND);
+        postCommands.put("/admin/buyers/disable", CommandName.DISABLE_ACCESS_COMMAND);
+        postCommands.put("/admin/buyers/enable", CommandName.ENABLE_ACCESS_COMMAND);
+        postCommands.put("/admin/unavailableProducts", CommandName.ENABLE_PRODUCT_COMMAND);
+        postCommands.put("/admin/generate", CommandName.GENERATE_BUYER_COMMAND);
+        postCommands.put("/admin/mail", CommandName.ADMIN_SEND_MAIL_COMMAND);
+        postCommands.put("/products/edit", CommandName.ADMIN_EDIT_PRODUCTS_COMMAND);
+
+        postCommands.put("/manag/add-personal", CommandName.ADD_EMPLOYEE_COMMAND);
+        postCommands.put("/manag/delete", CommandName.DELETE_EMPLOYEE_COMMAND);
     }
 
     @Override
@@ -109,21 +109,23 @@ public class CommandFilter implements Filter {
             } else {
                 actionName = uri.substring(beginAction);
             }
-            Command command = null;
 
+            CommandName commandName = null;
             switch (httpRequest.getMethod().toUpperCase()) {
                 case "GET":
-                    command = getCommands.get(actionName);
+                    commandName = getCommands.get(actionName);
                     break;
                 case "POST":
-                    command = postCommands.get(actionName);
+                    commandName = postCommands.get(actionName);
                     break;
             }
-            if (command != null) {
+
+            try {
+                Command command = factory.getCommand(commandName);
                 httpRequest.setAttribute(AttributeConstant.COMMAND.value(), command);
                 command.setName(actionName);
                 filterChain.doFilter(servletRequest, servletResponse);
-            } else {
+            } catch (CommandException e) {
                 logger.error("It is impossible to create command handler object and use filter");
                 ResourceBundle rb = ResourceBundleUtil.getResourceBundle(httpRequest);
                 String errMes = rb.getString("app.error.requested") + uri + rb.getString("app.error.requested.not.found");
